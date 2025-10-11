9e074694d3434bdec6b9bda568d5138334291c50
sangeetmen <32362498+sangeetmen@users.noreply.github.com>
2025-10-11 14:47:47 +0530

    Create excecises.js

---
+++ excecises.js
@@ -0,0 +1,282 @@
+const sampleExercises = [
+    {
+      "id": 1,
+      "name": "Lying Chest Press Machine (Flat)",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Chest",
+        "Triceps",
+        "Shoulders"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 2,
+      "name": "Body Weighted Squats",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Quadriceps",
+        "Glutes",
+        "Core"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 3,
+      "name": "Treadmill",
+      "category": "Cardio",
+      "muscleGroups": [
+        "Legs",
+        "Cardiovascular"
+      ],
+      "type": "cardio",
+      "image": ""
+    },
+    {
+      "id": 4,
+      "name": "Freestyle Swimming",
+      "category": "Swimming",
+      "muscleGroups": [
+        "Full Body",
+        "Cardiovascular"
+      ],
+      "type": "swimming",
+      "image": ""
+    },
+    {
+      "id": 5,
+      "name": "Plank",
+      "category": "Core",
+      "muscleGroups": [
+        "Core",
+        "Shoulders"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 6,
+      "name": "Dynamic Stretching",
+      "category": "Mobility",
+      "muscleGroups": [
+        "Full Body"
+      ],
+      "type": "mobility",
+      "image": ""
+    },
+    {
+      "id": 7,
+      "name": "Deadlift",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Hamstrings",
+        "Glutes",
+        "Back"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 8,
+      "name": "Pull-ups",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Back",
+        "Biceps"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 9,
+      "name": "Cycling",
+      "category": "Cardio",
+      "muscleGroups": [
+        "Legs",
+        "Cardiovascular"
+      ],
+      "type": "cardio",
+      "image": ""
+    },
+    {
+      "id": 10,
+      "name": "Backstroke Swimming",
+      "category": "Swimming",
+      "muscleGroups": [
+        "Full Body",
+        "Cardiovascular"
+      ],
+      "type": "swimming",
+      "image": ""
+    },
+    {
+      "id": 11,
+      "name": "Machine Fly (Front)",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Chest",
+        "Shoulders"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 12,
+      "name": "Lat Pull Down (Front)",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Lats",
+        "Back",
+		"Triceps"
+      ],
+      "type": "strength",
+      "image": ""
+    }, 
+	{
+      "id": 13,
+      "name": "Preacher Curl Machine",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Biceps"
+      ],
+      "type": "strength",
+      "image": ""
+    }, 
+	{
+      "id": 14,
+      "name": "Triceps Dip Machine",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Triceps"
+      ],
+      "type": "strength",
+      "image": ""
+    }, 
+	{
+      "id": 15,
+      "name": "Dumbbell Front Raises",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Shoulders",
+		"Chest",
+		"Biceps"
+      ],
+      "type": "strength",
+      "image": ""
+    }, 
+	{
+      "id": 16,
+      "name": "Dumbbell Side (Lat) Raises",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Lats",
+		"Shoulder"		
+      ],
+      "type": "strength",
+      "image": ""
+    }, 
+	{
+      "id": 17,
+      "name": "Dumbbell Bicep Curls",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Biceps"		
+      ],
+      "type": "strength",
+      "image": ""
+    }, 
+	{
+      "id": 18,
+      "name": "Dumbbell Hammer Curls",
+      "category": "Upper Body",
+      "muscleGroups": [
+        "Biceps"		
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 19,
+      "name": "Leg Press Machine",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Quadriceps",
+        "Glutes",
+        "Hamstrings"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 20,
+      "name": "Lunges",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Quadriceps",
+        "Glutes",
+        "Hamstrings"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 21,
+      "name": "Leg Curl Machine",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Calves",
+        "Glutes",
+        "Hamstrings"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 22,
+      "name": "Leg Extension Machine",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Quadriceps",
+        "Glutes",
+        "Hamstrings"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 23,
+      "name": "Standing Calf Raises",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Calves",
+        "Gastrocnemius",
+        "Soleus"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 24,
+      "name": "Sitting Calf Raises",
+      "category": "Lower Body",
+      "muscleGroups": [
+        "Calves",
+        "Soleus"
+      ],
+      "type": "strength",
+      "image": ""
+    },
+    {
+      "id": 25,
+      "name": "Crosfit (Eliptical)",
+      "category": "Cardio",
+      "muscleGroups": [
+        "Legs",
+        "Cardiovascular"
+      ],
+      "type": "cardio",
+      "image": ""
+    }
+  ]
